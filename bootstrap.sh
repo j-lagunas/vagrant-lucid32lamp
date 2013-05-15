980a633559054a0ce671d7a1cfcddf87dc7d543c
José Juan Lagunas <jlagunas@live.com.mx>
2013-05-15 15:03:22 -0400

    Password mysql agregada como variable. Creacion de usuario remoto en mysql

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,16 @@
 # if apache2 does no exist
 if [ ! -f /etc/apache2/apache2.conf ];
 then
+	
+	#Mysql Password
+	mysql_pwd="vagrant"
+
 	apt-get update 
 	apt-get install -y apache2
 
 	# Install MySQL
-	echo 'mysql-server-5.1 mysql-server/root_password password vagrant' | debconf-set-selections
-	echo 'mysql-server-5.1 mysql-server/root_password_again password vagrant' | debconf-set-selections
+	echo "mysql-server-5.1 mysql-server/root_password password $mysql_pwd" | debconf-set-selections
+	echo "mysql-server-5.1 mysql-server/root_password_again password $mysql_pwd" | debconf-set-selections
 	apt-get -y install mysql-client mysql-server-5.1
 
 	# Install PHP5 support
@@ -27,7 +31,15 @@ then
 	# Restart services
 	/etc/init.d/apache2 restart
 
+	#Create link to www
 	rm -rf /var/www
 	ln -fs /vagrant /var/www
+
+	#Add new remote user on mysql
+	mysql -uroot -p$mysql_pwd 
+	use mysql
+	GRANT ALL ON *.* to root@'192.168.10.1' IDENTIFIED BY 'vagrant';
+	FLUSH PRIVILEGES;
+	exit
 
 fi
